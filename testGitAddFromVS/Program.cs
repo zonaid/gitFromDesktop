51c9cae53aead12b32fa8a4572e56c595e3fa125
Zonaid Sharafi <zonaid@live.com>
2016-03-21 21:36:30 +1100

    This is sample from desktop

---
+++ testGitAddFromVS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testGitAddFromVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Git Zonaid Desktop");
+            Console.Read();
+        }
+    }
+}
